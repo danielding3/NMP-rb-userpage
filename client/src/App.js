0c44837cb427f0ddb0085b5665bcddfd0fb1a989
Rachel Yu <mx745796657@gmail.com>
2021-10-05 22:00:23 +1100

    setup page templat

---
+++ client/src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+import Car from './components/Car/Car';
+import Navbar from './components/Navbar/Navbar';
+import Profile from './components/Profile/Profile';
+import Teams from './components/Team/Teams'
+import TrackDay from './components/TrackDay/TrackDay';
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <div className="container">
+        <div className="leftContainer">
+          <Profile />
+          <Teams />
+        </div>
+        <div className="rightContainer">
+          <Car />
+          <TrackDay />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
