5d4a1ee0c0ae899919772335bef3ad6c3a3294c1
toughTitmouse <mx745796657@gmail.com>
2021-10-16 21:12:44 +1100

    change import (due to baseurl in tsconfig)

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,9 @@
 import './App.css';
-import Car from './components/Car/Car';
-import Navbar from './components/Navbar/Navbar';
-import Profile from './components/Profile/Profile';
-import Teams from './components/Team/Teams';
-import TrackDay from './components/TrackDay/TrackDay';
+import Car from 'components/Car/Car';
+import Navbar from 'components/Navbar/Navbar';
+import Profile from 'components/Profile/Profile';
+import Teams from 'components/Team/Teams';
+import TrackDay from 'components/TrackDay/TrackDay';
 function App(): JSX.Element {
   return (
     <div className="App">
