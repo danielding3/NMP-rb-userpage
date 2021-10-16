14e3612b2ddaba000456f7d7a1637143984a353e
toughTitmouse <mx745796657@gmail.com>
2021-10-16 21:13:11 +1100

    static profile section done

--- client/src/components/Profile/Profile.tsx
+++ client/src/components/Profile/Profile.tsx
@@ -1,7 +1,26 @@
 import styles from './Profile.module.css';
+import pic from 'assets/profile.png';
+import Button from '@mui/material/Button';
+import StyledEngineProvider from '@mui/material/StyledEngineProvider';
 
 const Profile = (): JSX.Element => {
-  return <div className={styles.root}>profile</div>;
+  return (
+    <StyledEngineProvider injectFirst>
+      <div className={styles.root}>
+        <div className={styles.picContainer}>
+          <img src={pic} alt="profile picture" className={styles.pic} />
+        </div>
+        <div className={styles.infoContainer}>
+          <div className={styles.username}>Jane Doe</div>
+          <div className={styles.updateProfile}>
+            <Button variant="text" className={styles.settingBtn}>
+              Profile Settings
+            </Button>
+          </div>
+        </div>
+      </div>
+    </StyledEngineProvider>
+  );
 };
 
 export default Profile;
