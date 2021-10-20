e0d95a83171edb1b8cd9b8e68dbd477b963a8222
toughTitmouse <mx745796657@gmail.com>
2021-10-20 12:34:54 +1100

    add profile edit dialog when click on profile settings

--- client/src/components/Profile/Profile.tsx
+++ client/src/components/Profile/Profile.tsx
@@ -1,9 +1,21 @@
+import React from 'react';
 import styles from './Profile.module.css';
 import pic from 'assets/profile.png';
 import Button from '@mui/material/Button';
 import StyledEngineProvider from '@mui/material/StyledEngineProvider';
+import ProfileEdit from './ProfileEdit';
 
 const Profile = (): JSX.Element => {
+  const [isEditProfile, setIsEditProfile] = React.useState(false);
+
+  const handleOpen = (): void => {
+    setIsEditProfile(true);
+  };
+
+  const handleClose = (): void => {
+    setIsEditProfile(false);
+  };
+
   return (
     <StyledEngineProvider injectFirst>
       <div className={styles.root}>
@@ -13,9 +25,14 @@ const Profile = (): JSX.Element => {
         <div className={styles.infoContainer}>
           <div className={styles.username}>Jane Doe</div>
           <div className={styles.updateProfile}>
-            <Button variant="text" className={styles.settingBtn}>
+            <Button
+              variant="text"
+              className={styles.settingBtn}
+              onClick={handleOpen}
+            >
               Profile Settings
             </Button>
+            <ProfileEdit open={isEditProfile} handleClose={handleClose} />
           </div>
         </div>
       </div>
