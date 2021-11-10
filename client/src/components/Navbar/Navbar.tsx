ba35a7facde7f9bced0e97fef901aade1c645e8a
Rachel Yu <mx745796657@gmail.com>
2021-11-11 00:03:22 +1100

    add comments for image import

--- client/src/components/Navbar/Navbar.tsx
+++ client/src/components/Navbar/Navbar.tsx
@@ -3,6 +3,11 @@ import logo from 'assets/logo.png';
 import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
 import StyledEngineProvider from '@mui/material/StyledEngineProvider';
 
+// this is how u can use a image
+// add image to assets folder
+// import the image from it, name of image is up to u (example in line 2)
+// then use it in src attribute like line 14
+
 const Navbar = (): JSX.Element => {
   return (
     <StyledEngineProvider injectFirst>
