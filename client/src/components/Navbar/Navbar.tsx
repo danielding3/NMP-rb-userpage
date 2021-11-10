4fa22535ec6ac75e24bcc2d02d9c031f4243b839
Khadus2412 <patelshubh882@gmail.com>
2021-11-10 22:57:29 +1100

    added the basics for header

--- client/src/components/Navbar/Navbar.tsx
+++ client/src/components/Navbar/Navbar.tsx
@@ -2,7 +2,27 @@ import styles from './Navbar.module.css';
 import React from 'react';
 
 const Navbar = (): JSX.Element => {
-  return <div className={styles.root}>navbar</div>;
+  return (
+    <div className={styles.root}>
+      <div>
+        <img src="images/logo.png" />
+        <nav>
+          <ul>
+            <li>
+              <a href="">HOME</a>
+            </li>
+            <li>
+              <a href="">REGION</a>
+            </li>
+            <li>
+              <a href="">ABOUT</a>
+            </li>
+          </ul>
+        </nav>
+        <img src="images/menu.png" />
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
